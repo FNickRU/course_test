7d7c894cb8e2d43a362cf075fdb57d0f4cc32a80
Stanislav Mayorov <mayorov-s-a@yandex.ru>
2018-06-17 22:33:49 +0700

    Решение пятнадцатой задачи

--- src/test/java/ru/parsentev/task_015/CycleShiftTest.java
+++ src/test/java/ru/parsentev/task_015/CycleShiftTest.java
@@ -1,21 +1,20 @@
 package ru.parsentev.task_015;
 
-import org.junit.Ignore;
-import org.junit.Test;
-import ru.parsentev.task_014.ReversePhrase;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
- * TODO: comment
+ * Тест циклического сдвига массива на N элементов вправо.
  *
  * @author parsentev
  * @since 28.07.2016
  */
-@Ignore
 public class CycleShiftTest {
-
+    /**
+     * [1, 2, 3, 4, 5] - shift(2) - [4, 5, 1, 2, 3].
+     */
     @Test
     public void multi() {
         CycleShift cycle = new CycleShift(new int[] {1, 2, 3, 4, 5});
@@ -23,6 +22,9 @@ public class CycleShiftTest {
         assertThat(result, is(new int[] {4, 5, 1, 2, 3}));
     }
 
+    /**
+     * [1] - shift(2) - [1].
+     */
     @Test
     public void one() {
         CycleShift cycle = new CycleShift(new int[] {1});
