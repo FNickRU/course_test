ab27538e5345f1163051d5534315dc091052d6b1
Stanislav Mayorov <mayorov-s-a@yandex.ru>
2018-06-24 14:37:21 +0700

    Решение семнадцатой задачи

--- src/main/java/ru/parsentev/task_017/CashMachine.java
+++ src/main/java/ru/parsentev/task_017/CashMachine.java
@@ -1,21 +1,76 @@
 package ru.parsentev.task_017;
 
+import static java.lang.Integer.signum;
+import static java.util.Arrays.sort;
+import static java.util.Collections.singletonList;
+import static org.apache.commons.lang3.ArrayUtils.reverse;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
- * TODO: comment
+ * Класс машины для размена бумажной купюры на монеты.
  *
- * @author parsentev
- * @since 28.07.2016
+ * @author fnickru
+ * @since 22.06.2018
  */
 public class CashMachine {
+    /**
+     * Номиналы монет в разменной машине.
+     */
     private final int[] values;
 
+    /**
+     * Конструктор с параметром.
+     * @param values - номиналы монет, доступных в машине
+     */
     public CashMachine(final int[] values) {
         this.values = values;
+        sort(this.values);
+        reverse(this.values);
     }
 
-    public List<List<Integer>> exchange(int note) {
-        throw new UnsupportedOperationException();
+    /**
+     * Метод размена банкноты на монеты.
+     * @param note - номинал банкноты, которую нужно разменять
+     * @return все варианты размена банкноты
+     */
+    public List<List<Integer>> exchange(final int note) {
+        return exchange(note, 0);
+    }
+
+    /**
+     * Метод размена остатка банкноты на монеты, при ограничении в максимальный номинал монет.
+     * @param residue - остаток, который необходимо разменять
+     * @param maxCoinIdx - индекс монеты с максимальным номиналом
+     * @return все варинты размена остатка
+     */
+    public List<List<Integer>> exchange(final int residue,
+                                        final int maxCoinIdx) {
+        List<List<Integer>> variations = new LinkedList<>();
+
+        for (int idx = maxCoinIdx; idx < values.length; ++idx) {
+            int value = values[idx];
+            Integer change = residue - values[idx];
+            switch (signum(change.compareTo(0))) {
+                case -1:
+                    break;
+                case 0:
+                    variations.add(new LinkedList<>(singletonList(value)));
+                    break;
+                case 1:
+                    for (List<Integer> variation : exchange(change, idx)) {
+                        if (!variation.isEmpty()) {
+                            variation.add(0, value);
+                            variations.add(variation);
+                        }
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException();
+            }
+        }
+
+        return variations;
     }
 }
--- src/test/java/ru/parsentev/task_017/CashMachineTest.java
+++ src/test/java/ru/parsentev/task_017/CashMachineTest.java
@@ -1,25 +1,25 @@
 package ru.parsentev.task_017;
 
-import org.junit.Ignore;
-import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import java.util.List;
 
 /**
- * TODO: comment
+ * Тест машины по размену купюр на монеты.
  *
  * @author parsentev
  * @since 28.07.2016
  */
-@Ignore
 public class CashMachineTest {
+    /**
+     * Если в машине монеты [1, 5, 10],
+     * а на размен подается купюра [1],
+     * то единственный вариант размена - [1].
+     */
     @Test
     public void unchange() {
         CashMachine machine = new CashMachine(new int[] {1, 5, 10});
@@ -27,6 +27,12 @@ public class CashMachineTest {
         assertThat(result, is(singletonList(singletonList(1))));
     }
 
+    /**
+     * Если в машине монеты [1, 5, 10],
+     * а на размен подается купюра [10],
+     * то варианты размена:
+     * [10], [5, 5], [5, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1].
+     */
     @Test
     public void change() {
         CashMachine machine = new CashMachine(new int[] {1, 5, 10});
